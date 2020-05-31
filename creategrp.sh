fad5a3a5aa78cd99d00c3d11f3c19b31a2ad02c2
YoungBrain <42530085+Khondakar@users.noreply.github.com>
2020-05-31 21:28:01 +1000

    Script for - Create Groups

---
+++ creategrp.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#Note: Create Groups
+#Author:Khondakar
+
+sudo addgroup grp1
+sudo addgroup grp2
+sudo addgroup grp3
+#	sudo addgroup nsw
+#	sudo addgroup qld
+echo "--------- Groups Successfully Created! -----------"
