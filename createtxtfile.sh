de2f06c3e72ffecb96787e9c804b60cb7b59c836
YoungBrain <42530085+Khondakar@users.noreply.github.com>
2020-05-31 21:29:52 +1000

    Script for - create folders and .txt files

---
+++ createtxtfile.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#Note: Create TXT files
+#Author:Khondakar
+
+sudo mkdir /home/khondakar/workSpace/myScript/apps
+sudo mkdir /home/khondakar/workSpace/myScript/apps/crm
+sudo mkdir /home/khondakar/workSpace/myScript/apps/crm/crmapp
+sudo mkdir /home/khondakar/workSpace/myScript/apps/crm/crmapp/main
+echo "-----* FOlders successfully created *----"
+sudo touch /home/khondakar/workSpace/myScript/apps/crm/crmapp/file1.txt
+sudo touch /home/khondakar/workSpace/myScript/apps/crm/crmapp/file2.txt
+sudo touch /home/khondakar/workSpace/myScript/apps/crm/crmapp/file3.txt
+sudo touch /home/khondakar/workSpace/myScript/apps/crm/crmapp/main/file1.txt
+sudo touch /home/khondakar/workSpace/myScript/apps/crm/crmapp/main/file2.txt
+echo "------* Files successfully created *-----"
