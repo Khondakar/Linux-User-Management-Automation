2e7a9ff0007039a7520a5858a7f66c3e409aa752
YoungBrain <42530085+Khondakar@users.noreply.github.com>
2020-06-02 19:57:34 +1000

    Script for - Create Users & Set Passwords

--- adduser.sh
+++ adduser.sh
@@ -3,19 +3,19 @@
 # Author: Khondakar
 
 # Add users:
-sudo useradd -m auto1
-sudo useradd -m auto2
-sudo useradd -m auto3
+sudo useradd -m Usr1
+sudo useradd -m Usr2
+sudo useradd -m Usr3
 #	sudo cp -r /etc/skel/. /auto1
 #	sudo cp -r /etc/skel/. /auto2
 #	sudo cp -r /etc/skel/. /auto3
 
 # Set user passwords:
-echo "Please set password for user - 'auto1' :"
-sudo passwd auto1
-echo "Please set password for user - 'auto2' :"
-sudo passwd auto2
-echo "Please set password for user - 'auto3' :"
-sudo passwd auto3
+echo "Please set password for user - 'Usr1' :"
+sudo passwd Usr1
+echo "Please set password for user - 'Usr2' :"
+sudo passwd Usr2
+echo "Please set password for user - 'Usr3' :"
+sudo passwd Usr3
 echo "-----------------------------------"
 echo "Users creted & Password set!"
