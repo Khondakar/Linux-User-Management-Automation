0fd570807895947c81536bd0970320eebb1ba728
YoungBrain <42530085+Khondakar@users.noreply.github.com>
2020-06-02 11:43:14 +1000

    Script for-deleting user and user's home directory

--- deleteuser.sh
+++ deleteuser.sh
@@ -2,11 +2,15 @@
 #Note: Delete existing users
 #Author:Khondakar
 
-sudo userdel auto1
+sudo userdel auto1 
 sudo userdel auto2
 sudo userdel auto3
 
-sudo rm -r /home/auto1
-sudo rm -r /home/auto2
-sudo rm -r /home/auto3
+sudo userdel -r auto1 # '-r' option is deleting user's home directory
+sudo userdel -r auto2
+sudo userdel -r auto3
+
+#sudo rm -r /home/auto1
+#sudo rm -r /home/auto2
+#sudo rm -r /home/auto3
 echo "-----* Users succeessfully Deleted *---------."
