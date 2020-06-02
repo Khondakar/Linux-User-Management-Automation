2c6039d27b37f966b10ed505110b51094d0366eb
YoungBrain <42530085+Khondakar@users.noreply.github.com>
2020-06-02 11:38:30 +1000

    Script for - deleting users.

--- deleteuser.sh
+++ deleteuser.sh
@@ -6,7 +6,7 @@ sudo userdel auto1
 sudo userdel auto2
 sudo userdel auto3
 
-sudo rm -r /auto1
-sudo rm -r /auto2
-sudo rm -r /auto3
+sudo rm -r /home/auto1
+sudo rm -r /home/auto2
+sudo rm -r /home/auto3
 echo "-----* Users succeessfully Deleted *---------."
