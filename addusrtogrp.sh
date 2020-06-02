aeabd4650a02575475abf313a8c85ef78551f1d4
YoungBrain <42530085+Khondakar@users.noreply.github.com>
2020-06-02 19:58:49 +1000

    Script for - Add Users to the Groups

--- addusrtogrp.sh
+++ addusrtogrp.sh
@@ -2,8 +2,8 @@
 # Note: Add users to the groups.
 # Author: Khondakar
 
-sudo usermod -a -G grp1 auto1
-sudo usermod -a -G grp2 auto2
-sudo usermod -a -G grp3 auto3
+sudo usermod -a -G grp1 Usr1
+sudo usermod -a -G grp2 Usr2
+sudo usermod -a -G grp3 Usr3
 
 echo "---- Users being added to the Groups --------"
