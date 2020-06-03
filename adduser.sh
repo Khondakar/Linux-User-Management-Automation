64dd2b8cabb66508ab6ea84c57092ec1397f8885
YoungBrain <42530085+Khondakar@users.noreply.github.com>
2020-06-03 17:05:40 +1000

    Script for - Add Users & Set Passwords.

--- adduser.sh
+++ adduser.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
-# Note: Add users & set password.
-# Author: Khondakar
+#Note: Add users & set password.
+#Author: Khondakar
 
-# Add users:
-sudo useradd -m Usr1
-sudo useradd -m Usr2
-sudo useradd -m Usr3
+# Create Users:
+echo -e "\e[33mHow many User you want to create?"
+read input
+
+# For loop use to create as many user as you want
+for ((i=1; i<= $input; i++))
+do
+ sudo useradd -m Usr$i; # Create user based on the input value
+done
+echo -e "\e[33m--- Users Successfully Created -----"
+
+# Create users:
+#sudo useradd -m Usr1
+#sudo useradd -m Usr2
+#sudo useradd -m Usr3
 #	sudo cp -r /etc/skel/. /auto1
 #	sudo cp -r /etc/skel/. /auto2
 #	sudo cp -r /etc/skel/. /auto3
 
-# Set user passwords:
-echo "Please set password for user - 'Usr1' :"
+# Assign password for the users:
+echo -e "\e[33mPlease set password for user - 'Usr1' :"
 sudo passwd Usr1
-echo "Please set password for user - 'Usr2' :"
+echo -e "\e[33mPlease set password for user - 'Usr2' :"
 sudo passwd Usr2
-echo "Please set password for user - 'Usr3' :"
+echo -e "\e[33mPlease set password for user - 'Usr3' :"
 sudo passwd Usr3
-echo "-----------------------------------"
-echo "Users creted & Password set!"
+echo -e "\e[34m------ Users Created & Passwords being set!! --------"
