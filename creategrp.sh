68ac0348392b019dc4dd84abff5ae52d3314bcb4
YoungBrain <42530085+Khondakar@users.noreply.github.com>
2020-06-03 17:07:19 +1000

    Script for - Create Groups

--- creategrp.sh
+++ creategrp.sh
@@ -2,9 +2,20 @@
 #Note: Create Groups
 #Author:Khondakar
 
-sudo addgroup grp1
-sudo addgroup grp2
-sudo addgroup grp3
+# User input:
+echo -e "\e[33mHow many Group you want to create?"
+read input
+
+# Create groups based on user input
+for ((c=1; c<= $input; c++))
+do
+ sudo addgroup grp$c;
+done
+
+
+#sudo addgroup grp1
+#sudo addgroup grp2
+#sudo addgroup grp3
 #	sudo addgroup nsw
 #	sudo addgroup qld
-echo "--------- Groups Successfully Created! -----------"
+echo -e "\e[33m---------* Groups Successfully Created! *-----------"
